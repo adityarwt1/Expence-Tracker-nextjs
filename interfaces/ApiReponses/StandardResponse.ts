51280cab77d12c689f5ed106e4cbe18467f3b959
Aditya Rawat <adityarawatnew2487@gmail.com>
2026-01-30 00:44:56 +0530

    api response writtend

--- interfaces/ApiReponses/StandardResponse.ts
+++ interfaces/ApiReponses/StandardResponse.ts
@@ -1,3 +1,8 @@
+import { HttpStatusCode, HttpStatusText } from "@/enums/HttpStatusCodeAndStatus";
+
 export interface StandardApiResponse {
-    
+    success:boolean,
+    status:HttpStatusCode,
+    error?:HttpStatusText | string,
+    message?:string
 }
---
+++ utils/apiResponses.ts
@@ -0,0 +1,46 @@
+"use server"
+
+import { HttpStatusCode, HttpStatusText } from "@/enums/HttpStatusCodeAndStatus"
+import { StandardApiResponse } from "@/interfaces/ApiReponses/StandardResponse"
+import { NextResponse } from "next/server"
+
+export const badRequest = async (message:string)=> NextResponse.json<StandardApiResponse>({
+    status:HttpStatusCode.BAD_REQUEST,
+    success:false,
+    error:HttpStatusText.BAD_REQUEST,
+    message:message
+},
+{
+    status:HttpStatusCode.BAD_REQUEST
+})
+
+export const internalServerIssue = async (error?: Error)=> NextResponse.json<StandardApiResponse>({
+    status:HttpStatusCode.INTERNAL_SERVER_ERROR,
+    success:false,
+    error: (error as Error).message || HttpStatusText.INTERNAL_SERVER_ERROR,
+    message:"Internal server issue!"
+},
+{
+    status:HttpStatusCode.INTERNAL_SERVER_ERROR
+})
+
+export const enexpectedError  = async (message?:string)=> NextResponse.json<StandardApiResponse>({
+    status:HttpStatusCode.INTERNAL_SERVER_ERROR,
+    success:false,
+    error:HttpStatusText.INTERNAL_SERVER_ERROR,
+    message:message || "Unexpected Error!"
+},
+{
+    status:HttpStatusCode.INTERNAL_SERVER_ERROR
+})
+
+export const conflictError = async (message?:string)=> NextResponse.json<StandardApiResponse>({
+    status:HttpStatusCode.CONFLICT,
+    success:false,
+    error:HttpStatusText.CONFLICT,
+    message:message || "Already exist!"
+},
+{
+    status:HttpStatusCode.CONFLICT
+}
+)
