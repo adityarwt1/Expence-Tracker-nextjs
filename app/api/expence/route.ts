1e381ff32d7bd85e88e2d4b1c1aff7915b2fe2ee
Aditya Rawat <adityarawatnew2487@gmail.com>
2026-01-31 23:35:42 +0530

    completed the main expence

--- app/api/expence/route.ts
+++ app/api/expence/route.ts
@@ -1,4 +1,7 @@
+import { HttpStatusCode } from "@/enums/HttpStatusCodeAndStatus";
 import { ExpenceAddInterface, ExpenceResponseInterface } from "@/interfaces/ApiReponses/v1/expence/expencesInterfaces";
+import { mongoconnect } from "@/lib/mongodb";
+import Expence from "@/models/Expence";
 import { badRequest, internalServerIssue } from "@/utils/apiResponses";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -11,7 +14,25 @@ export async function POST(req:NextRequest):Promise<NextResponse<ExpenceResponse
             return badRequest("filed not provided properly!")
         }
 
-        
+        const isconnected  = await mongoconnect()
+
+        if(!isconnected){
+            return internalServerIssue(new Error("Failed to connect Database!"))
+        }
+
+        const expence = await Expence.create(body)
+
+        if(!expence){
+            return internalServerIssue(new Error("Failed to create expence!"))
+        }
+
+        return NextResponse.json({
+            status:HttpStatusCode.CREATED,
+            success:true,
+        },
+    {
+        status:HttpStatusCode.CREATED
+    })
     } catch (error) {
         console.log(error)
         return internalServerIssue(error as Error)
